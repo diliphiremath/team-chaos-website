b3582ce3ae07240d6c342246d931c4ef278685b0
Dilip Hiremath <45544469+diliphiremath@users.noreply.github.com>
2020-12-18 23:25:08 +0530

    added logo

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+import team_chaos from './team_chaos.png';
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="logo">
+      <img src={team_chaos} alt="this is car image" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
